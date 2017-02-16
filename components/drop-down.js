245d4f91b86214d239d69039f63942c81dc167f4
Mark Brown <markbrown4@gmail.com>
2017-02-16 16:09:17 +0000

    use :global

--- components/drop-down.js
+++ components/drop-down.js
@@ -26,12 +26,12 @@ export default class DropDown extends Component {
         onClick={this.toggle.bind(this)}
       >
         { children }
-        <style jsx global>{`
+        <style jsx>{`
         .drop-down {
           position: relative;
           cursor: pointer;
         }
-        .drop-down ul {
+        .drop-down :global(ul) {
           position: absolute;
           z-index: 2;
           top: -9999px;
@@ -41,14 +41,14 @@ export default class DropDown extends Component {
           border: 1px solid rgba(0,0,0, .1);
           box-shadow: 0 1px 2px rgba(0,0,0, .1);
         }
-        .drop-down.active ul {
+        .drop-down.active :global(ul) {
           top: 100%;
         }
-        .drop-down .align-right {
+        .drop-down :global(.align-right) {
           left: auto;
           right: 0;
         }
-        .drop-down a {
+        .drop-down :global(a) {
           display: block;
           padding: 3px 20px;
           color: #333;
@@ -56,7 +56,7 @@ export default class DropDown extends Component {
           line-height: 1;
           white-space: nowrap;
         }
-        .drop-down a:hover {
+        .drop-down :global(a:hover) {
           background: #eee;
         }
         `}</style>
